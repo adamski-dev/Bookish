86b399c599669e04c8cfb2833f45e48551513fbb
adamski-dev <adam.poplawski.programista@gmail.com>
2023-01-30 08:05:01 +0000

    addition of nav-bar component

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    id:              number;
+    title:           string;
+    author:          string;
+    description:     string;
+    price:           number;
+    pictureUrl:      string;
+    productCategory: string;
+    productType:     string;
+}
